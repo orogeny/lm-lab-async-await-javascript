f9869d63054efc8739fae47263c7cc0d420aa8f1
Paul <paulwharin@gmail.com>
2023-11-07 09:30:38 +0000

    feat: chain successful response handler and error block

--- exercise001.js
+++ exercise001.js
@@ -12,3 +12,6 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // Your solution(s) to exercise001 here!
+
+promise.then((resp) => console.log("Yay! Promise resolved with response: " + resp))
+.catch((err) => console.log("Boo. Promise rejected with response: " + err));
