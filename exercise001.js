6918aba3199691d11c5d0c0c2847e1deb335754b
Paul <paulwharin@gmail.com>
2023-11-07 12:34:10 +0000

    chore: add empty prettier config and reformatted exercise001.js

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,14 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
 // Your solution(s) to exercise001 here!
@@ -18,13 +18,12 @@ const promise = new Promise((resolve, reject) => {
 
 const getPromiseResponse = async () => {
   try {
-  const resp = await promise;
+    const resp = await promise;
 
-  console.log("Yay! Promise resolved with response: " + resp);
-  }
-  catch (err) {
+    console.log("Yay! Promise resolved with response: " + resp);
+  } catch (err) {
     console.log("Boo. Promise rejected with response: " + err);
   }
-}
+};
 
 getPromiseResponse();
